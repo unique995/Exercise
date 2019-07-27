0b7d67834d3f637bcadfca53a23c9d093b8f7d7e
unique995 <1255418138@qq.com>
2019-07-27 19:16:32 +0800

    'exercise'

---
+++ src/review/Tree/levelOrder.java
@@ -0,0 +1,44 @@
+package review.Tree;
+
+/*
+给定一个 N 叉树，返回其节点值的层序遍历。 (即从左到右，逐层遍历)。
+ */
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+public class levelOrder {
+    public static List LevelOrder(Node node){
+        Queue<Node> queue = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if (node == null)
+            return result;
+        queue.add(node);
+        while (!queue.isEmpty()){
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            while (size-- > 0){
+                Node n = queue.poll();
+                list.add(n.val);
+                queue.addAll(n.children);
+            }
+            result.add(list);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+
+    }
+}
