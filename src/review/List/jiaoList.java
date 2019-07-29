76e914fbfcf2f396e0373446e464ec59f6118c39
unique995 <1255418138@qq.com>
2019-07-29 16:53:49 +0800

    '求相交链表的交点'

---
+++ src/review/List/jiaoList.java
@@ -0,0 +1,49 @@
+package review.List;
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+public class jiaoList {
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null){
+            return null;
+        }
+        int lenA = 0,lenB = 0;
+        ListNode curA = headA,curB = headB;
+        while (curA != null){
+            lenA++;
+            curA = curA.next;
+        }
+        while (curB != null){
+            lenB++;
+            curB = curB.next;
+        }
+        ListNode longNode = null,shortNode = null;
+        if (lenA >= lenB){
+            longNode = headA;
+            shortNode = headB;
+        }else {
+            longNode = headB;
+            shortNode = headA;
+        }
+        int diff = Math.abs(lenA-lenB);
+        while (diff > 0){
+            longNode = longNode.next;
+            diff--;
+        }
+        while (longNode != null){
+            if (longNode.equals(shortNode)) return longNode;
+            longNode = longNode.next;
+            shortNode = shortNode.next;
+        }
+        return null;
+    }
+    public static void main(String[] args) {
+
+    }
+}
