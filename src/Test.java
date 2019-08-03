e111037fb8aabcf9fd311c8d31017cf1dd162e08
unique995 <1255418138@qq.com>
2019-08-03 17:30:25 +0800

    'exercise'

---
+++ src/Test.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = sc.nextInt();
+//        while (sc.hasNext()){
+//            for (int i = 0; i < num;i++){
+//                int sum = sc.nextInt();
+//                int []array = new int[sum];
+//                for (int j = 0;j < array.length;j++){
+//                    array[j] = sc.nextInt();
+//                }
+//                if (array.length > 2){
+//                    int x ;
+//                    for (x = 0;x < array.length-2;){
+//                        if (array[x] < array[x+1]+array[x+2]){
+//                            x++;
+//                        }
+//                    }
+//                    if (array[x] < array[x+1]+array[0] && array[x+1] < array[0]+array[1]){
+//                        System.out.println("YES");
+//                    }else {
+//                        System.out.println("NO");
+//                    }
+//
+//                }else
+//                    System.out.println("NO");
+//            }
+//        }
+//        Scanner scanner = new Scanner(System.in);
+//        int num = scanner.nextInt();
+//        int []arr = new int[num];
+//        while (scanner.hasNext()){
+//            for (int i = 0;i < num;i++){
+//                arr[i] = scanner.nextInt();
+//            }
+//            for (int x = 0;x < arr.length;x++){
+//                for (int y = 1;y < arr.length;y++){
+//                    if ((arr[x]+arr[y]) % 2 != 0){
+//                        int temp = arr[x];
+//                        arr[x] = arr[y];
+//                        arr[y] = temp;
+//                    }
+//                }
+//            }
+//            for (int j = 0;j < arr.length;j++){
+//                System.out.print(arr[j]+" ");
+//            }
+//        }
+        Scanner scanner = new Scanner(System.in);
+        int sum = scanner.nextInt();
+        int [] array = new int[sum];
+        for (int i = 0;i < sum;i++){
+            array[i] = scanner.nextInt();
+        }
+        int num = scanner.nextInt();
+        while (num != 0){
+            for (int j = 0;j < num;j++){
+
+            }
+        }
+    }
+}
