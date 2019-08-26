154d2ad3fc14bd3a3988a43f7657706dd7c2454c
unique995 <1255418138@qq.com>
2019-08-26 16:02:34 +0800

    '最长回文子串'

--- src/LeetCode/longestSubstring.java
+++ src/LeetCode/longestSubstring.java
@@ -1,6 +1,7 @@
 package LeetCode;
 
 public class longestSubstring {
+    static int i = 0;
     public int lengthOfLongestSubstring(String s) {
         int []hash = new int[500];
         int fast = 0;
@@ -17,5 +18,35 @@ public class longestSubstring {
             }
         }
         return max;
+    }
+    public String findPalindrome(String s, int left, int right) {
+        int n = s.length();
+        int l = left;
+        int r = right;
+        while (l >= 0 && r <= n - 1 && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return s.substring(l + 1, r);
+    }
+    public String LongestPalindrome(String s){
+        int n = s.length();
+        if (n<=1) return s;
+
+        String longest = "";
+
+        String str;
+        for (int i=0; i<n-1; i++) {
+            str = findPalindrome(s, i, i);
+            if (str.length() > longest.length()){
+                longest = str;
+            }
+            str = findPalindrome(s, i, i + 1);
+            if (str.length() > longest.length()){
+                longest = str;
+            }
+        }
+
+        return longest;
     }
 }
