f96b4735df0170ca54a2da6599426f4a17e54dad
unique995 <1255418138@qq.com>
2019-09-04 20:52:32 +0800

    'exercise'

--- src/NewCoder/diver.java
+++ src/NewCoder/diver.java
@@ -1,5 +1,8 @@
 package NewCoder;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class diver {
     public int divide(int dividend, int divisor) {
 
@@ -21,5 +24,53 @@ public class diver {
             }
         }
         return flag ? -count : count;
+    }
+
+    public static int Num(int x){
+        if (x == 0){
+            return 0;
+        }
+        if (x == 1 ){
+            return 1;
+        }
+        if (x == 2){
+            return 1;
+        }
+        return Num(x-1)+Num(x-2);
+    }
+    public static int Num1(int x){
+        int a = 1;
+        int b = 1;
+        int result = 0;
+        if (x > 2){
+            for (int i = 3;i <= x;i++){
+                result = a + b;
+                a = b;
+                b = result;
+            }
+        }else if (x == 0){
+            return 0;
+        }else {
+            return 1;
+        }
+        return result;
+    }
+    static int schedule(int m,int[] array) {
+        int []min = new int[m];
+        int x = 0;
+            for (int j = 0;j < array.length;j++){
+                if (m < array.length-j){
+                    min[x] = array[j]+array[j+1];
+                    m--;
+                }else
+                    min[x++] = array[j];
+            }
+        Arrays.sort(min);
+        return min[0];
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(Num1(x));
     }
 }
